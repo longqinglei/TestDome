7eb4679b1afca99dac0addf64878338679eb4ac4
龙青磊 <longqinglei@test.com>
2017-08-09 21:30:08 +0800

    一个测试用的Dome

---
+++ TestDome/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  TestDome
+//
+//  Created by 龙青磊 on 2017/8/9.
+//  Copyright © 2017年 龙青磊. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ TestDome/ViewController.m
@@ -0,0 +1,30 @@
+//
+//  ViewController.m
+//  TestDome
+//
+//  Created by 龙青磊 on 2017/8/9.
+//  Copyright © 2017年 龙青磊. All rights reserved.
+//
+
+#import "ViewController.h"
+
+@interface ViewController ()
+
+@end
+
+@implementation ViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view, typically from a nib.
+    NSLog(@"我就是我 不一样的烟火");
+}
+
+
+- (void)didReceiveMemoryWarning {
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+
+@end
